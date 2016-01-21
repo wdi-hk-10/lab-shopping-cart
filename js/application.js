16bf5abfc4238b09bae04583988fbc75ce6cd5a9
Shawn Alwani <shawn.alwani@gmail.com>
2016-01-21 20:34:17 +0800

    completed add new item

---
+++ js/application.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+function newShoppingItem (){
+  $("#addNewItem").on("click", function createEntry (){
+    var goods = $("#inputItem").val();
+    var goodsprice = $("#inputPrice").val();
+    var newrow = $("<div>").addClass("row");
+    var newname = $("<div>").addClass("col-md-3").text(goods);
+    var newprice = $("<div>").addClass("col-md-3").text("$"+goodsprice);
+    var newlabel = $("<label>").text("QTY");
+    var newinput = $("<input>").attr("value","0");
+    var newbutton = $("<button>").addClass("enter").text("Cancel");
+    var newqty = $("<div>").addClass("col-md-3").append(newlabel).append(newinput).append(newbutton);
+    var newsubtotal = $("<div>").addClass("col-md-3").text("$0.00");
+    var whole = newrow.append(newname).append(newprice).append(newqty).append(newsubtotal);
+    $("#entrypoint").prepend(whole);
+  });
+}
+newShoppingItem();
+
+
+
+
+
+});
