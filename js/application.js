b1dd8271472ac6c1dcbb15ef11d0b2ed15cc277b
Shawn Alwani <shawn.alwani@gmail.com>
2016-01-21 23:36:05 +0800

    added total function

--- js/application.js
+++ js/application.js
@@ -6,32 +6,51 @@ function newShoppingItem (){
     var goodsprice = $("#inputPrice").val();
     var newrow = $("<div>").addClass("row");
     var newname = $("<div>").addClass("col-md-3").text(goods);
-    var newprice = $("<div>").addClass("col-md-3").text("$"+goodsprice);
+    var newprice = $("<div>").addClass("col-md-3").addClass("pricing").text("$"+goodsprice);
     var newlabel = $("<label>").text("QTY");
-    var newinput = $("<input>").attr("value","0");
+    var newinput = $("<input>").addClass("enterqty").attr("value","0");
     var newbutton = $("<button>").addClass("enter").text("Cancel");
     var newqty = $("<div>").addClass("col-md-3").append(newlabel).append(newinput).append(newbutton);
-    var newsubtotal = $("<div>").addClass("col-md-3").text("$0.00");
+    var newsubtotal = $("<div>").addClass("col-md-3").addClass("subtots").text("$0.00");
     var whole = newrow.append(newname).append(newprice).append(newqty).append(newsubtotal);
     $("#entrypoint").prepend(whole);
   });
 }
 
 function removeShoppingItem (){
-    $("#entrypoint").on("click",".enter", function deleteEntry (){
-      $(this).parent().parent().remove();
-    });
+  $("#entrypoint").on("click",".enter", function deleteEntry (){
+    $(this).parent().parent().remove();
+    $(".btn").click();
+  });
 }
 
-
-
-
-
+function calculateTotals () {
+  $(".btn").on("click", function update (){
+    var pricearray=[];
+    var qtyarray=[];
+    var subtotalarray = [];
+    var check = $(".pricing").text().split("$");
+    check.shift();
+    for (var i=0;i<check.length; i++){
+      pricearray[i]=parseFloat(check[i]);
+    }
+    qtyarray = $('.enterqty').map(function(){return $(this).val();}).get();
+    var finaltotal=0;
+    for (var x=0;x<check.length;x++){
+      subtotalarray[x] = pricearray[x]*qtyarray[x];
+      finaltotal = finaltotal+subtotalarray[x];
+    }
+    var counter=0;
+    $(".subtots").map(function(){
+      $(this).text("$"+subtotalarray[counter]);
+      counter = counter+1;
+    })
+    $("#totalPrice").text("$"+finaltotal);
+  });
+}
 
 newShoppingItem();
 removeShoppingItem();
-
-
-
+calculateTotals();
 
 });
