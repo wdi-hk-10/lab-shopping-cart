f4ee433218cb922c32b8bd17c071d4bc58dfc4f7
RedCodster <rob_fong00@yahoo.com>
2016-01-22 09:25:25 +0800

    fixed the format

--- js/application.js
+++ js/application.js
@@ -1,22 +1,37 @@
 $(document).ready (function()  {
 
 
-
+var =
 function updatePrice () {
 
 
 }
-var =
+
+// onClick of "Calculate Prices" button
+// calculate the "item-sub" for each "item-row" by multiplying "item-qty" by "item-price"
+// sum the "item-sub" field for each "item-row" and return value for "totalPrice" in footer
+
+
 function createRecord () {
 
 
 }
 
+// onClick of "Create" button
+// take input of "item-name" and "item-price"
+// create a new "item-row" with the inputs
+// append to the "item-list" div
 
-function addRecord () {
+
+
+function removeRecord () {
 
 
 }
+
+// onClick of "Cancel" button
+// remove or hide all the inputs of "item-row" div from the "item-list" div
+// hide contents of "item-row" to prevent awkward spacing
 
 
 
